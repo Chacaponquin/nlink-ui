bcd528507389d66ecf6f00aa6d8d99a442de50df
Chacaponquin <hectorangel2001@gmail.com>
2022-12-01 23:10:16 +0000

    Change Java Init Project

---
+++ src/main/Nlink.java
@@ -0,0 +1,14 @@
+package main;
+
+import ui.UI;
+/**
+ *
+ * @author Hector Angel Gomez
+ */
+public class Nlink {
+
+    public static void main(String[] args) {
+        UI main = new UI();
+        main.setVisible(true);   
+    }
+}
